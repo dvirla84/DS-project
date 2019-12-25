a57fc7194efb4b4cd3b5d981c1aaeb14c359496c
dvirla84 <58984659+dvirla84@users.noreply.github.com>
2019-12-25 11:57:02 +0200

    dvir

--- Graph_Node.cpp
+++ Graph_Node.cpp
@@ -1,8 +1,8 @@
 #include "Graph_Node.h"
 Graph_Node::Graph_Node(unsigned key):
         Node(key),
-        _Out(new List<Graph_Node>),
-        _In(new List<Graph_Node>){}
+        _Out(new List<Graph_Node*>),
+        _In(new List<Graph_Node*>){}
 
 Graph_Node:: ~Graph_Node()
 {
--- Graph_Node.h
+++ Graph_Node.h
@@ -6,8 +6,8 @@
 #include "Node.h"
 class Graph_Node: public Node {
 private:
-    List<Graph_Node>* _Out;
-    List<Graph_Node>* _In;
+    List<Graph_Node*>* _Out;
+    List<Graph_Node*>* _In;
 public:
     Graph_Node(unsigned key);
     unsigned Get_out_Degree() const {return _Out->getSize();}
