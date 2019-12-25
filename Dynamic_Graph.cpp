2ca1186eaaa8516e2de4c938fe4b257ce43f5f64
EB <eitan.b@campus.technion.ac.il>
2019-12-25 12:09:31 +0200

    Rooted_Tree destructor update change names

--- Dynamic_Graph.cpp
+++ Dynamic_Graph.cpp
@@ -6,3 +6,26 @@ _nodesSize(0),
 _edgesSize(0),
 _edges(new List<Graph_Edge*>)
 {}
+
+Dynamic_Graph:: ~Dynamic_Graph()
+{
+    delete _nodes;
+    delete _edges;
+}
+
+Graph_Node* Dynamic_Graph::  Insert_Node(unsigned node_key)
+{
+    Graph_Node* node = new Graph_Node(node_key);
+    _nodes->pushBack(node);
+    return node;
+}
+
+void Dynamic_Graph::  Delete_Node(Graph_Node* node){}
+
+Graph_Edge*  Dynamic_Graph:: Insert_Edge(Graph_Node* from, Graph_Node* to){}
+
+void Dynamic_Graph:: Delete_Edge(Graph_Edge* edge){}
+
+Rooted_Tree* Dynamic_Graph:: SCC() const {}
+
+Rooted_Tree*  Dynamic_Graph:: BFS(Graph_Node* source) const{}
--- Dynamic_Graph.h
+++ Dynamic_Graph.h
@@ -4,6 +4,7 @@
 #include "List.h"
 #include "Graph_Node.h"
 #include "Graph_Edge.h"
+#include "Rooted_Tree.h"
 
 class Dynamic_Graph {
 private:
@@ -14,6 +15,13 @@ private:
 
 public:
     Dynamic_Graph();
+    ~Dynamic_Graph();
+    Graph_Node* Insert_Node(unsigned node_key);
+    void Delete_Node(Graph_Node* node);
+    Graph_Edge* Insert_Edge(Graph_Node* from, Graph_Node* to);
+    void Delete_Edge(Graph_Edge* edge);
+    Rooted_Tree* SCC() const;
+    Rooted_Tree* BFS(Graph_Node* source) const;
 };
 
 
