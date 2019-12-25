1c74b03999e18d8c75cc9e1943524d119a9929b3
EB <eitan.b@campus.technion.ac.il>
2019-12-25 11:32:03 +0200

    Rooted_Tree destructor update change names

--- Graph_Node.cpp
+++ Graph_Node.cpp
@@ -1,1 +1,6 @@
 #include "Graph_Node.h"
+Graph_Node:: ~Graph_Node()
+{
+    delete _Out;
+    delete _In;
+}
--- Graph_Node.h
+++ Graph_Node.h
@@ -6,11 +6,12 @@
 #include "Node.h"
 class Graph_Node: public Node {
 private:
-    List<Graph_Node> _Out;
-    List<Graph_Node> _In;
+    List<Graph_Node> *_Out;
+    List<Graph_Node> *_In;
 public:
-    unsigned Get_out_Degree() const {return _Out.getSize();}
-    unsigned Get_in_Degree() const {return _In.getSize();}
+    ~Graph_Node();
+    unsigned Get_out_Degree() const {return _Out->getSize();}
+    unsigned Get_in_Degree() const {return _In->getSize();}
 
 };
 
