76b1c262201be52d4c220abf12534b6744108504
EB <eitan.b@campus.technion.ac.il>
2020-01-03 18:16:49 +0200

    BFS 2 try

--- Dynamic_Graph.cpp
+++ Dynamic_Graph.cpp
@@ -75,64 +75,128 @@ void Dynamic_Graph::BFS_init(unsigned* colorArr, Graph_Node* source, List<Graph_
     graphQueue->pushFront(source);
 }
 
-Rooted_Tree*  Dynamic_Graph:: BFS(Graph_Node* source) const
-{
-    unsigned* colorArr = new unsigned[_insertTime];
-    List<Graph_Node*>* graphQueue = new List<Graph_Node*>;
-    List<Tree_Node*>* treeQueue = new List<Tree_Node*>;
-    BFS_init(colorArr,source, graphQueue);
-    Tree_Node* root = new Tree_Node(source->Get_key());
-    Rooted_Tree* rootedTree = new Rooted_Tree();
-    rootedTree->setRoot(root);
-    treeQueue->pushFront(root);
-
-    while(graphQueue->getSize() > 0)
-    {
-        Graph_Node* u = graphQueue->popBack();
-        Tree_Node* parent = treeQueue->popBack();
-        unsigned outSize = u->Get_out_Degree();
-        if(outSize == 0) continue;
-        Graph_Edge* u_v = u->getOutList()->top();
-        Graph_Node* v = u_v->getTo();
-        int counter = 0;
-//        while(colorArr[v->getInsertionTime()] != WHITE)
+//Rooted_Tree*  Dynamic_Graph:: BFS(Graph_Node* source) const
+//{
+//    unsigned* colorArr = new unsigned[_insertTime];
+//    List<Graph_Node*>* graphQueue = new List<Graph_Node*>;
+//    List<Tree_Node*>* treeQueue = new List<Tree_Node*>;
+//    BFS_init(colorArr,source, graphQueue);
+//    Tree_Node* root = new Tree_Node(source->Get_key());
+//    Rooted_Tree* rootedTree = new Rooted_Tree();
+//    rootedTree->setRoot(root);
+//    treeQueue->pushFront(root);
+//
+//    while(graphQueue->getSize() > 0)
+//    {
+//        Graph_Node* u = graphQueue->popBack();
+//        Tree_Node* parent = treeQueue->popBack();
+//        unsigned outSize = u->Get_out_Degree();
+//        if(outSize == 0) continue;
+//        Graph_Edge* u_v = u->getOutList()->top();
+//        Graph_Node* v = u_v->getTo();
+//        int counter = 0;
+////        while(colorArr[v->getInsertionTime()] != WHITE)
+////        {
+////            if(u_v->getOutPointer()->getPrevious() != NULL)
+////            {
+////                u_v = u_v->getOutPointer()->getPrevious()->getItem();
+////                v = u_v->getTo();
+////                counter++;
+////            }
+////            else break;
+////        }
+//        Tree_Node* rightestSibling = new Tree_Node(v->Get_key());
+////        if(counter == outSize)
+////            delete rightestSibling;
+//
+//        for (unsigned i = counter; i < outSize ; i++)
 //        {
+//            if(colorArr[v->getInsertionTime()] == WHITE)
+//            {
+//                if(parent->getLeftChild() != NULL) {
+//                    colorArr[v->getInsertionTime()] = GRAY;
+//                    graphQueue->pushFront(v);
+//                    Tree_Node *brother = new Tree_Node(v->Get_key());
+//                    brother->setParent(parent);
+//                    rightestSibling->setRightSibling(brother);
+//                    rightestSibling = brother;
+//                    treeQueue->pushFront(rightestSibling);
+//                }
+//                else
+//                    parent->setLeftChild(rightestSibling);
+//            }
 //            if(u_v->getOutPointer()->getPrevious() != NULL)
 //            {
 //                u_v = u_v->getOutPointer()->getPrevious()->getItem();
 //                v = u_v->getTo();
-//                counter++;
 //            }
-//            else break;
 //        }
-        Tree_Node* rightestSibling = new Tree_Node(v->Get_key());
-//        if(counter == outSize)
-//            delete rightestSibling;
+//        colorArr[u->getInsertionTime()] = BLACK;
+//    }
+//    delete treeQueue;
+//    delete graphQueue;
+//    delete[] colorArr;
+//    return rootedTree;
+//}
 
-        for (unsigned i = counter; i < outSize ; i++)
+
+
+
+Rooted_Tree*  Dynamic_Graph:: BFS2(Graph_Node* source) const
+{
+    unsigned* colorArr = new unsigned[_insertTime];
+    List<Graph_Node*>* graphQueue = new List<Graph_Node*>;
+    List<Tree_Node*>* treeQueue = new List<Tree_Node*>;
+    Tree_Node* root = new Tree_Node(source->Get_key());
+    BFS_init(colorArr,source, graphQueue);
+    Rooted_Tree* rootedTree = new Rooted_Tree();
+    rootedTree->setRoot(root);
+    treeQueue->pushFront(root);
+    while(graphQueue->getSize() > 0)
+    {
+        Graph_Node *u = graphQueue->popBack();
+        Tree_Node *parent = treeQueue->popBack();
+        Graph_Edge *u_v = u->getOutList()->top();
+        unsigned outSize =u->getOutList()->getSize();
+        Tree_Node *rightestSibling;
+        Graph_Node *v = u_v->getTo();
+        for(int i = 0; i < outSize ; i++)
         {
-            if(colorArr[v->getInsertionTime()] == WHITE)
-            {
-                if(parent->getLeftChild() != NULL) {
-                    colorArr[v->getInsertionTime()] = GRAY;
-                    graphQueue->pushFront(v);
-                    Tree_Node *brother = new Tree_Node(v->Get_key());
-                    brother->setParent(parent);
-                    rightestSibling->setRightSibling(brother);
-                    rightestSibling = brother;
-                    treeQueue->pushFront(rightestSibling);
+            if (colorArr[v->getInsertionTime()] != WHITE) {
+                if(u_v->getSelfPointer()->getPrevious()!= NULL)
+                {
+                    u_v = u_v->getSelfPointer()->getPrevious()->getItem();
+                    v = u_v->getTo();
                 }
-                else
-                    parent->setLeftChild(rightestSibling);
+                continue;
             }
-            if(u_v->getOutPointer()->getPrevious() != NULL)
+            if (parent->getLeftChild() == NULL) {
+                Tree_Node *leftChild = new Tree_Node(v->Get_key());
+                parent->setLeftChild(leftChild);
+                leftChild->setParent(parent);
+                rightestSibling = leftChild;
+                treeQueue->pushFront(leftChild);
+            }
+            else
+                {
+                Tree_Node *sibling = new Tree_Node(v->Get_key());
+                rightestSibling->setRightSibling(sibling);
+                sibling->setParent(parent);
+                rightestSibling = sibling;
+                treeQueue->pushFront(sibling);
+            }
+            colorArr[v->getInsertionTime()] = GRAY;
+            graphQueue->pushFront(v);
+            if(u_v->getSelfPointer()->getPrevious()!= NULL)
             {
-                u_v = u_v->getOutPointer()->getPrevious()->getItem();
+                u_v = u_v->getSelfPointer()->getPrevious()->getItem();
                 v = u_v->getTo();
             }
+
         }
         colorArr[u->getInsertionTime()] = BLACK;
     }
+
     delete treeQueue;
     delete graphQueue;
     delete[] colorArr;
